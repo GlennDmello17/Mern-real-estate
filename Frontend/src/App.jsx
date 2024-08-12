3c56478ab033b72ca39b33297e062d61a50ee791
GlennDmello17 <glenndmello17@gmail.com>
2024-08-12 17:50:24 +0530

    added pages and router #commit no 2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-       <h1 className="text-red-500">
-      Hello world!
-    </h1>
-    </>
+    <BrowserRouter>
+        <Routes>
+             <Route path='/' element={<Home/>}/>
+             <Route path='/sign-in' element={<SignIn/>}/>
+             <Route path='/sign-up' element={<SignUp/>}/>
+             <Route path='/about' element={<About/>}/>
+             <Route path='/profile' element={<Profile/>}/>
+            
+             
+        </Routes>
+    </BrowserRouter>
   )
 }
 
