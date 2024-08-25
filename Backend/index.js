0b097eb10867ae78c34fbac6cc899675da27a8f9
GlennDmello17 <glenndmello17@gmail.com>
2024-08-25 11:37:02 +0530

    creating middleware

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,30 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './Routes/user.route.js';
+import authRouter from './Routes/auth.route.js';
+dotenv.config();
+mongoose.connect(process.env.mongo).then(()=>{
+    console.log("connected to dbS")
+}).catch((err)=>{
+    console.log(err)
+});
 const app=express()
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log("server running on 3000")
+});
+
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'internal server error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    });
 });
